d7d1d7fc72e1de6a31c200478424fa027c6dfa31
Thomas Bunch <56885257+tbunch-datalogics@users.noreply.github.com>
2020-05-17 09:25:08 -0500

    cleaned up the file structure, put sketches in their own folder

--- app/components/root.js
+++ app/components/root.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Sketch from './Sketch';
-import SketchTest from './SketchTest';
-import Hex from './Hex';
+import Grid from './Grid';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 const App = () => {
@@ -10,6 +8,7 @@ const App = () => {
       <div className="App">
         <nav>
           Welcome!
+          <Link to="/test">Test</Link>
           <Link to="/upload">Upload</Link>
           <Link to="/maps">Maps</Link>
           <Link to="/">Home</Link>
@@ -20,13 +19,7 @@ const App = () => {
         </h1>
         {/* <p>This seems like a nice place to get started with some Routes!</p> */}
         <Switch>
-          <Route exact path="/upload">
-            <SketchTest />
-          </Route>
-          <Route exact path="/maps">
-            <Hex />
-          </Route>
-          <Route exact path="/" component={Sketch} />
+          <Route exact path="/" component={Grid} />
         </Switch>
       </div>
     </Router>
