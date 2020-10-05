4fb25a065097c1a7fdadf921100b137822d2d00d
Thomas Bunch <56885257+tbunch-datalogics@users.noreply.github.com>
2020-10-05 16:38:18 -0500

    played a bit with some of the drag and drop styling. struggling to get font awesome to work

--- app/components/ImageUpload.js
+++ app/components/ImageUpload.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchImageTest } from '../redux/image';
-import ReactDropzone from 'react-dropzone';
+import Dropzone from 'react-dropzone';
 
 const mapStateToProps = function (state) {
   return {
@@ -21,44 +21,40 @@ class ImageUploadTest extends React.Component {
     this.state = {
       image: null,
     };
-    this.fileChangeHandler = this.fileChangeHandler.bind(this);
     this.onImageChange = this.onImageChange.bind(this);
     this.uploadHandler = this.uploadHandler.bind(this);
   }
   render() {
     return (
-      <div id="file-upload">
-        <input
-          type="file"
-          onChange={this.onImageChange}
-          className="filetype"
-          id="group_image"
-        />
-      </div>
+      <Dropzone onDrop={(acceptedFiles) => this.onImageChange(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section className="upload-container">
+            <div {...getRootProps()} className="upload-box">
+              <input {...getInputProps()} />
+              <p>Drop your map here</p>
+              <i className="fas fa-upload"></i>
+            </div>
+          </section>
+        )}
+      </Dropzone>
     );
   }
   uploadHandler(event) {
     this.props.loadImage(this.state.image);
   }
 
-  onImageChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
+  onImageChange = (acceptedFiles) => {
+    if (acceptedFiles && acceptedFiles[0]) {
       let reader = new FileReader();
       reader.onload = (e) => {
         console.log('After the reader.onload starts', e);
         this.setState({ image: e.target.result });
         this.uploadHandler();
       };
-      console.log(
-        'Before the reader.ReadAsDataURL invoked',
-        event.target.files[0]
-      );
-      reader.readAsDataURL(event.target.files[0]);
+      console.log('Before the reader.ReadAsDataURL invoked', acceptedFiles[0]);
+      reader.readAsDataURL(acceptedFiles[0]);
     }
   };
-  fileChangeHandler(event) {
-    this.setState({ selectedFile: event.target.files[0] });
-  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ImageUploadTest);
